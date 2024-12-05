9189adce71df292375e1d94ac9eb18e456d106f5
penguin-cho <penguin-cho@cloud-latitude.com>
2024-12-05 23:22:09 +0800

    refactor: Remove unused Head import and clean up Home component structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
 import { Metadata } from "next";
-import Head from "next/head";
 import Image from "next/image";
 
 export const metadata: Metadata = {
@@ -10,7 +9,6 @@ export const metadata: Metadata = {
 export default function Home() {
   return (
     <>
-
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
           <Image
