90943441fff677f1a5865be184b32c0d2e1c6c86
penguin-cho <penguin-cho@cloud-latitude.com>
2025-03-15 15:37:07 +0800

    refactor: Integrate RwdComponent in ParticleSensorContent for responsive rendering of ProductCarousel and ServiceCarousel

--- src/components/solutions/ParticleSensorContent.tsx
+++ src/components/solutions/ParticleSensorContent.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { ProductCarousel } from './ProductCarousel';
+import RwdComponent from '../common/RwdComponent';
+import ServiceCarousel from '../common/CarouselWithNavList';
 
 const particleSensorProductImageList = [
     "/images/particle-sensor/product-1.jpg",
@@ -11,7 +13,10 @@ export function ParticleSensorContent() {
     return (
         <div>
             <h2 className="text-2xl font-bold mb-4">Particle Sensor Solution</h2>
-            <ProductCarousel productImageList={particleSensorProductImageList} />
+            <RwdComponent
+                mobileComponent={<ProductCarousel productImageList={particleSensorProductImageList} />}
+                desktopComponent={<ServiceCarousel imageList={particleSensorProductImageList} />}
+            />
             <p className="text-gray-600">
                 This is the content specific to the Particle Sensor solution.
                 {/* Add more content here */}
