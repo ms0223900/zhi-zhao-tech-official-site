317bf7cb0b32c3c4c7f00ef7b7748cdce44d655b
penguin-cho <penguin-cho@cloud-latitude.com>
2025-03-16 15:20:18 +0800

    feat: Add ProductAdvantageSection to ParticleSensorContent, showcasing product benefits with detailed descriptions and icons for enhanced user engagement

--- src/components/solutions/ParticleSensorContent.tsx
+++ src/components/solutions/ParticleSensorContent.tsx
@@ -5,6 +5,8 @@ import RwdComponent from '../common/RwdComponent';
 import ServiceCarousel from '../common/CarouselWithNavList';
 import MainContentContainer from '../common/MainContentContainer';
 import LinkCard from '../common/LinkCard';
+import SectionTitle from './SectionTitle';
+import ProductAdvantageCard from './ProductAdvantageCard';
 
 const particleSensorProductImageList = [
     "/images/particle-sensor/product-1.jpg",
@@ -48,6 +50,7 @@ export function ParticleSensorContent() {
                 </div>
             </section>
             <LinkSection />
+            <ProductAdvantageSection />
         </div>
     );
 }
@@ -68,5 +71,58 @@ function LinkSection() {
                 subtitle="廠房"
             />
         </div>
+    )
+}
+
+const productAdvantageList = [
+    {
+        title: "高性價比",
+        detailDescription: "市售量測工具單價高，本產品具備多項功能可供搭配",
+        iconSrc: "/images/particle-sensor/icon-cost-effective.svg"
+    },
+    {
+        title: "體積小",
+        detailDescription: "體積優勢適用各類型設備，可用於異物好發區域、機台腔體...等",
+        iconSrc: "/images/particle-sensor/icon-compact-size.svg"
+    },
+    {
+        title: "簡便",
+        detailDescription: "隨插隨用、方便安裝、擴增性佳",
+        iconSrc: "/images/particle-sensor/icon-easy-to-use.svg"
+    },
+    {
+        title: "7*24實時監測",
+        detailDescription: "貫徹工安第一、工區6S環境維護與整潔",
+        iconSrc: "/images/particle-sensor/icon-7-24-monitoring.svg"
+    },
+    {
+        title: "動態量測",
+        detailDescription: "生產過程同步量測，毋須停機",
+        iconSrc: "/images/particle-sensor/icon-dynamic-measurement.svg"
+    },
+    {
+        title: "無線傳輸",
+        detailDescription: "數據藉由WiFi回傳伺服器，省去佈線或手動檢測等潛在污染源",
+        iconSrc: "/images/particle-sensor/icon-wireless-transmission.svg"
+    }
+]
+
+// product advantage section
+function ProductAdvantageSection() {
+    return (
+        <section className="bg-white -mx-4 md:mx-0 px-4">
+            <SectionTitle title="產品優勢" />
+            <div className="container grid grid-cols-1 md:grid-cols-2 gap-4 py-6 ">
+                {productAdvantageList.map((item, index) => (
+                    <ProductAdvantageCard
+                        key={index}
+                        title={item.title}
+                        subtitle=""
+                        detailDescription={item.detailDescription}
+                        iconSrc={item.iconSrc}
+                    />
+                ))}
+            </div>
+        </section>
     )
 }
