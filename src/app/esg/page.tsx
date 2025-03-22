7f6c412fa1526ab7ce2c2b772f84c3d013377042
penguin-cho <penguin-cho@cloud-latitude.com>
2025-03-22 22:42:35 +0800

    refactor: Adjust Banner component in ESGPage for improved responsive design, including updated padding and image positioning

--- src/app/esg/page.tsx
+++ src/app/esg/page.tsx
@@ -28,10 +28,10 @@ export default function ESGPage() {
 
 const Banner = () => {
     return (
-        <section className="max-w-[1440px] mx-auto relative h-0 pt-[60%] xl:pt-[600px] overflow-hidden">
+        <section className="max-w-[1440px] mx-auto relative h-0 pt-[102%] md:pt-[60%] xl:pt-[600px] overflow-hidden">
             <div className="absolute inset-0">
                 <RwdComponent
-                    mobileComponent={<img src="/images/esg/esg-banner-mobile.png" alt="ESG" className="w-full h-full object-cover" />}
+                    mobileComponent={<img src="/images/esg/esg-banner-mobile.png" alt="ESG" className="w-full h-full object-cover translate-y-[-30%]" />}
                     desktopComponent={<img src="/images/esg/esg-banner-desktop.png" alt="ESG" className="max-w-[130%] xl:max-w-[100%] translate-y-[-30%] translate-x-[-50px] xl:translate-x-0" />}
                 />
 
