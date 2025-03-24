9a1b96a38a0bfc04fdbc6041f8d7ec83b79379a3
penguin-cho <penguin-cho@cloud-latitude.com>
2025-03-24 10:00:14 +0800

    refactor: Update SocialSection component layout and content for improved readability and structure, enhancing visual hierarchy and modularity

--- src/components/esg/SocialSection.tsx
+++ src/components/esg/SocialSection.tsx
@@ -8,16 +8,22 @@ export default function SocialSection() {
             <div className="border-2 border-[#E57B42] rounded-2xl overflow-hidden theme-gradient-blue">
                 <div className="flex flex-col md:flex-row gap-4 items-center px-2 md:px-12 py-10 md:py-6">
                     {/* 左側文字區塊 */}
-                    <div className="w-full md:w-[55%] flex flex-col md:flex-row justify-start items-center gap-4 md:gap-12">
-                        <div className="flex flex-col items-center">
-                            <h2 className="text-[60px] leading-none font-black text-[#E57B42] mb-2">S</h2>
-                            <h3 className="text-h2 text-primary-blue-dark md:text-2xl font-medium">企業社會責任</h3>
-                            <p className="text-h2 text-xl text-primary-blue-dark">Social</p>
+                    <div className="w-full md:w-[65%] flex-shrink-0 flex flex-col md:flex-row justify-start items-center gap-4 md:gap-12">
+                        <div className="flex flex-col items-center text-[#E57B42] ">
+                            <h2 className="text-[60px] leading-none font-black mb-2">S</h2>
+                            <h3 className="text-h2 md:text-2xl font-medium">企業社會責任</h3>
+                            <p className="text-h2 text-xl">Social</p>
                         </div>
 
                         <div className="space-y-4 flex flex-col items-center md:items-start">
-                            <h4 className="inline-block text-h2 text-xl font-medium border-b border-gray-400">社會公益</h4>
-                            <h4 className="inline-block text-h2 text-xl font-medium border-b border-gray-400">員工關懷</h4>
+                            {['榮總捐款', '翠屏國中小學校', '日僑學校', '偏遠地區弱勢家庭外衣衣物捐贈'].map((item, index) => (
+                                <h4
+                                    key={index}
+                                    className="inline-block text-h2 text-xl font-medium border-b border-gray-400"
+                                >
+                                    {item}
+                                </h4>
+                            ))}
                         </div>
                     </div>
 
